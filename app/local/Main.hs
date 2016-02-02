4d6492063ee9a9b6bc41f0c69bd574de7fccec7f
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-02-02 14:18:09 -0800

    prettify command line client

--- app/local/Main.hs
+++ app/local/Main.hs
@@ -5,61 +5,124 @@ module Main where
 import Hive
 
 import Control.Lens
+import Control.Monad
 import Control.Monad.IO.Class
+import Data.List.NonEmpty     (NonEmpty)
 import System.Console.ANSI
+import System.Console.Haskeline
+import Text.Megaparsec
+import Text.Printf
 
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Vector        as V
 
 data Turn
-    = Placement Bug BoardIndex
-    | Move [BoardIndex]
-    deriving Read
+    = Place Bug BoardIndex
+    | Move BoardIndex (NonEmpty BoardIndex)
 
 main :: IO ()
-main = runHive (consolePlayer "Player 1") (consolePlayer "Player 2")
+main = do
+    putStrLn ("To place a piece: " ++ colored White "place ant 0 1")
+    putStrLn ("To move a piece:  " ++ colored White "move 2 3, 2 4, 3 4")
 
-consolePlayer :: String -> Game -> Hive IO ()
+    runInputT defaultSettings
+        (runHive (consolePlayer "Player 1")
+                 (consolePlayer "Player 2"))
+
+consolePlayer :: String -> Game -> Hive (InputT IO) ()
 consolePlayer name game = do
     liftIO $ do
-        putStrLn (colored (player2color (game^.gamePlayer)) name ++ ", what's your move?")
+        putStrLn ""
         printBoard (game^.gameBoard)
 
-    mturn <- reads <$> lift getLine
-    case mturn of
-        [(turn, "")] -> do
-            result <-
-                case turn of
-                    Placement bug idx -> makePlacement bug idx
-                    Move (x:xs) -> makeMove x (NE.fromList xs)
-                    _ -> error "empty move"
-
-            case result of
-                Nothing -> do
-                    liftIO (putStrLn "invalid move!")
+    let prompt = colored (player2color (game^.gamePlayer)) (name ++ "> ")
+    lift (getInputLine prompt) >>= \case
+        Nothing   -> pure ()
+        Just line ->
+            case runParser turnParser "" line of
+                Left _ -> do
+                    liftIO (putStrLn (colored Red "Parse error."))
                     consolePlayer name game
-                Just (GameOver winner) -> liftIO (print winner)
-                Just (GameActive game') -> consolePlayer name game'
+                Right turn -> do
+                    result <-
+                        case turn of
+                            Place bug idx -> makePlacement bug idx
+                            Move x xs     -> makeMove x xs
 
-        _ -> do
-            liftIO (putStrLn "Parse error.")
-            consolePlayer name game
+                    case result of
+                        Nothing -> do
+                            liftIO (putStrLn (colored Red "Invalid move!"))
+                            consolePlayer name game
+                        Just (GameOver winner) -> liftIO (print winner)
+                        Just (GameActive game') -> consolePlayer name game'
+
+turnParser :: Parsec String Turn
+turnParser = placeParser <|> moveParser
+  where
+    placeParser :: Parsec String Turn
+    placeParser = do
+        _ <- string' "place"
+        space
+        bug <- bugParser
+        space
+        idx <- idxParser
+        pure (Place bug idx)
+
+    moveParser :: Parsec String Turn
+    moveParser = do
+        _ <- string' "move"
+        space
+        (x:xs) <- sepBy1 idxParser (space *> char ',' <* space)
+        pure (Move x (NE.fromList xs))
+
+    bugParser :: Parsec String Bug
+    bugParser =
+            Ant         <$ string' "ant"
+        <|> Grasshopper <$ string' "grasshopper"
+        <|> Spider      <$ string' "spider"
+        <|> Beetle      <$ string' "beetle"
+        <|> Queen       <$ string' "queen"
+
+    idxParser :: Parsec String BoardIndex
+    idxParser = do
+        row <- num
+        space
+        col <- num
+        pure (row, col)
+      where
+        num = read <$> some digitChar
 
 printBoard :: Board -> IO ()
-printBoard (HexBoard tiles parity) =
-    mapM_ printTwoRows (V.map (split . V.toList . V.map cellToString) tiles)
+printBoard board = do
+    putStrLn (colored White column_nums)
+    putStrLn ("   " ++ replicate (3 * (board^.boardWidth) - 2) '-')
+
+    V.imapM_
+        printTwoRows
+        (V.map (split . V.toList . V.map cellToString) (board^.boardTiles))
   where
-    printTwoRows :: ([String], [String]) -> IO ()
-    printTwoRows (es, os)
-        | parity == Even = do
-            putStrLn (spaceFirst os)
-            putStrLn (spaceSecond es)
+    column_nums :: String
+    column_nums =
+        "   " ++ concatMap (printf "%-2d ")
+                            (take (board^.boardWidth) [(0::Int)..])
+
+    printTwoRows :: Int -> ([String], [String]) -> IO ()
+    printTwoRows row (es, os)
+        | board^.boardParity == Even = do
+            unless (null os) $
+                putStrLn (row_num ++ spaceFirst os)
+            unless (null es) $
+                putStrLn (row_num ++ spaceSecond es)
         | otherwise = do
-            putStrLn (spaceSecond es)
-            putStrLn (spaceFirst os)
+            unless (null es) $
+                putStrLn (row_num ++ spaceSecond es)
+            unless (null os) $
+                putStrLn (row_num ++ spaceFirst os)
+      where
+        row_num = colored White (printf "%2d|" row)
 
     cellToString :: Cell -> String
-    cellToString [] = "-"
+    cellToString [] = "·"
     cellToString (Tile p bug : _) = colored (player2color p) [bug2char bug]
 
 bug2char :: Bug -> Char
